8ab8c58aad24c99638627a4e4e44fb100be01328
Adam Bowers <adambowers09@live.com>
2022-01-22 16:59:36 -0500

    update

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Password-Generator
 
+## About
+
+This website will generate a secure password using confirms and prompts from a defined length that can include lowercase, uppercase, numbers, and symbols.  
+
+## GitHub Repo & Website Link
+
+[Link to website](https://adambowers09.github.io/password-generator/)
+
+[Link to Github Repo](https://github.com/adambowers09/password-generator)
+
+## Screenshot
+
+![screentsho of application about my paget](./assets/images/screencapture-03Challenge.png)
+
+
 
--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,67 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
+  
+}
+
+function generatePassword () {
+  var actualPassword = "";
+  
+  
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var num = "0123456789";
+  var symbol = "~!@#$%^&*()<>?";
+
+  var requiredChar = "";
+
+  var length = prompt("Please enter a number!")
+    while (length < 8 ||length > 128) {
+      length = prompt("You need at least 8 character and no more than 128");
+    }
+
+  var confirmLc = confirm("Would you lowercase letters?"); 
+    if (confirmLc) {
+      requiredChar = requiredChar.concat(lowerCase);
+    }
+     
+  var confirmUc = confirm("Would you like uppercase letters?");
+    if (confirmUc) {
+      requiredChar = requiredChar.concat(upperCase);
+    }
+
+  var confirmNum = confirm("Would you like numbers?");
+    if (confirmNum) {
+      requiredChar = requiredChar.concat(num);
+    }
+
+  var confirmSymbol = confirm("Would you like special characters?");
+    if (confirmSymbol) {
+      requiredChar = requiredChar.concat(symbol);
+    }
+  
+
+ for (var i = 0; i < length; i++) {
+   var random = Math.floor(Math.random() * requiredChar.length);
+
+   actualPassword = actualPassword.concat(
+     requiredChar.charAt(random)
+   );
+ }
+
+return actualPassword;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
