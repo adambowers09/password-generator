e4ef2a6fee05b7bf81ec48ee637a55b739435fbe
Adam Bowers <adambowers09@live.com>
2022-01-23 18:45:58 -0500

    Final Updates

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// String
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
@@ -21,30 +22,35 @@ function writePassword() {
 function generatePassword () {
   var actualPassword = "";
   
-  
+
   var requiredChar = "";
 
 
+  // Prompt  
   var length = prompt("Please enter a number!")
     while (length < 8 ||length > 128) {
       length = prompt("You need at least 8 character and no more than 128");
     }
 
+  // Confirm Lowercase  
   var confirmLc = confirm("Would you lowercase letters?"); 
     if (confirmLc) {
       requiredChar = requiredChar.concat(lowerCase);
     }
-     
+    
+  // Confirm Uppercase    
   var confirmUc = confirm("Would you like uppercase letters?");
     if (confirmUc) {
       requiredChar = requiredChar.concat(upperCase);
     }
 
+  // Confirm Numbers  
   var confirmNum = confirm("Would you like numbers?");
     if (confirmNum) {
       requiredChar = requiredChar.concat(num);
     }
 
+  // Confirm Symbols  
   var confirmSymbol = confirm("Would you like special characters?");
     if (confirmSymbol) {
       requiredChar = requiredChar.concat(symbol);
